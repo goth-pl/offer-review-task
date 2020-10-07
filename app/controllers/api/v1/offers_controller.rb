41a88b5f8d6e8786898bae43c8b9c8d0f2f858ff
goth-pl <m.szycko@gmail.com>
2020-10-07 14:42:32 +0200

    Filtered model attributes in json response.

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::OffersController < ApplicationController
   before_action :set_offer, only: [:show, :accept, :reject]
 
   def index
-    render json: { offers: policy_scope(Offer.all) }
+    offers = policy_scope(Offer.includes(:products, :salesman, :client))
+
+    render json: { offers: prepare_offers_to_render(offers) }
   end
 
   def show
     authorize @offer
 
-    render json: { offer: @offer }
+    render json: { offer: prepare_offers_to_render(@offer) }
   end
 
   def create
@@ -16,7 +18,7 @@ class Api::V1::OffersController < ApplicationController
 
     command = CreateOffer.new
 
-    command.on(:create_offer_successful) { |offer| render json: { offer: offer }, status: :created }
+    command.on(:create_offer_successful) { |offer| render json: { offer: prepare_offers_to_render(offer) }, status: :created }
     command.on(:create_offer_failed) { |errors| render json: { errors: errors }, status: :bad_request }
 
     command.call(offer_params)
@@ -27,7 +29,7 @@ class Api::V1::OffersController < ApplicationController
 
     command = AcceptOffer.new
 
-    command.on(:accept_offer_successful) { |offer| render json: { offer: offer } }
+    command.on(:accept_offer_successful) { |offer| render json: { offer: prepare_offers_to_render(offer) } }
     command.on(:accept_offer_failed) { |errors| render json: { errors: errors }, status: :bad_request }
 
     command.call(@offer)
@@ -38,13 +40,24 @@ class Api::V1::OffersController < ApplicationController
 
     command = RejectOffer.new
 
-    command.on(:reject_offer_successful) { |offer| render json: { offer: offer } }
+    command.on(:reject_offer_successful) { |offer| render json: { offer: prepare_offers_to_render(offer) } }
     command.on(:reject_offer_failed) { |errors| render json: { errors: errors }, status: :bad_request }
 
     command.call(@offer)
   end
 
   private
+
+  def prepare_offers_to_render(offers)
+    offers.as_json(
+      include: [
+        { products: { except: [:id, :created_at, :updated_at, :offer_id] } },
+        { salesman: { only: [:email] } },
+        { client: { only: [:email] } }
+      ],
+      except: [:salesman_id, :client_id, :updated_at]
+    )
+  end
 
   def set_offer
     @offer = Offer.find(params[:id])
